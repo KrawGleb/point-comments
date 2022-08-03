52d8d910201766e482f1b10f08ac4cbb8cc1661c
KrawGleb <krawcevitsch@gmail.com>
2022-08-03 14:44:42 +0300

    Add gulp

---
+++ PointComments.WebUI/wwwroot/ts/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+ 
+var tsProject = ts.createProject('tsconfig.json');
+ 
+gulp.task('scripts', function() {
+    var tsResult = gulp.src("app/**/*.ts") // or tsProject.src()
+        .pipe(tsProject());
+ 
+    return tsResult.js.pipe(gulp.dest('release'));
+});
+ 
+// gulp.task('watch', ['scripts'], function() {
+//     gulp.watch('lib/*.ts', ['scripts']);
+// });
