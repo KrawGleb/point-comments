8fda3d09177997b85656d6347f21f20e2bab0544
KrawGleb <Krawcevitsch@gmail.com>
2022-08-03 16:38:57 +0300

    webpack works

---
+++ PointComments.WebUI/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: "./scripts/app.ts",
+    output: {
+        path: path.resolve(__dirname, "wwwroot"),
+        filename: "[name].[chunkhash].js",
+        publicPath: "/",
+    },
+    resolve: {
+        extensions: [".js", ".ts"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+};
--- PointComments.WebUI/wwwroot/js/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var point_service_1 = require("./services/point.service");
-var pointService = new point_service_1.PointService();
-console.log(pointService.getAll());
-//# sourceMappingURL=app.js.map
